dba43c389e29d0b9fc26445c299715927ea84ed3
Haralampos Protoolis <harry@192-168-1-3.tpgi.com.au>
2022-11-30 21:13:57 +0800

    initial commit

---
+++ AddModuleBookmark.js
@@ -0,0 +1,5 @@
+(()=>{
+  var kgp = document.createElement('script'); 
+  kgp.src = 'https://cdn.jsdelivr.net/gh/vl20100/KGProgressBars@0.0.1.d/dist/KGP.js'; 
+  kgp.id = 'kpgscript_TriggerNotify'; document.head.appendChild(kgp); 
+})()
---
+++ README.md
@@ -0,0 +1,3 @@
+# HP Derivative Clicker
+
+a local fork of [derivative clicker](https://github.com/gzgreg/DerivativeClicker/tree/gh-pages) and a series of personal scripts
