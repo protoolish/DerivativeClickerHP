a5ec0a101e267e141e79944f6c710b7ac6da85a8
Haralampos Protoolis <protoolish@gmail.com>
2022-11-30 23:18:25 +0800

    wip

--- AddModuleBookmark.js
+++ AddModuleBookmark.js
@@ -1,6 +1,6 @@
 (()=>{
   let el_script = document.createElement('script');
-  el_script.src = 'https://cdn.jsdelivr.net/gh/protoolish/DerivativeClickerHP/api.js';
-  el_script.id = 'script_HPDCMain';
+  el_script.src = 'https://cdn.jsdelivr.net/gh/protoolish/DerivativeClickerHP/api.min.js';
+  el_script.id = 'script_TriggerNotify';
   document.head.appendChild(el_script);
 })()
